ea2e2b4f39dec9e231033beb07f6b01d2a16cb44
Fitim Bytyqi <fitim.bytyqi@ornio.no>
2021-06-29 16:11:25 +0200

    REST-13 Added LoginUser service

--- src/controllers/LoginUser.controller.ts
+++ src/controllers/LoginUser.controller.ts
@@ -4,6 +4,9 @@ import { Request, Response, NextFunction, Router } from 'express';
 // middlewares
 import { ValidationMiddleware } from '../middleware/Validation.middleware';
 
+// services
+import { LoginUserService } from '../services/LoginUser.service';
+
 // validators
 import { LoginUserValidator } from '../validators/LoginUser.validator';
 
@@ -16,7 +19,7 @@ LoginUserController.post(
         try {
             const { email, password } = req.body.input;
 
-            // const result = await LoginUserService.login(email, password);
+            const result = await LoginUserService.login(email, password);
         } catch (err) {
             console.error(err);
         }
---
+++ src/services/LoginUser.service.ts
@@ -0,0 +1,10 @@
+export const LoginUserService = {
+    login: async (email: string, password: string) => {
+        // Check if user with this email exists
+        console.log('Testing Service');
+
+        // check if password is valid
+
+        // generate the token
+    },
+};
