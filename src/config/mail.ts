14608b6beb80d9dac1559377fd4c22d092c3703e
albrimpaqarizi <albrim.paqarizi@ornio.no>
2021-07-12 22:33:25 +0200

    REST-49 Fixed bug for send email

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -3,8 +3,9 @@ export const MAIL_HOST = process.env.MAIL_HOST || 'smtp.mailhog.ornio.xyz';
 export const MAIL_PORT = process.env.MAIL_PORT || 1025;
 export const MAIL_USERNAME = process.env.MAIL_USERNAME;
 export const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+export const MAIL_ENCRYPTION = process.env.MAIL_ENCRYPTION;
 export const MAIL_FROM_NAME = process.env.MAIL_FROM_NAME || 'RealEstate';
-export const MAIL_FROM_ADDRESS = process.env.MAIL_FROM_ADDRESS || 'noreply.realestate@gmail.com';
+export const MAIL_FROM_ADDRESS = process.env.MAIL_FROM_ADDRESS || 'noreply-realestate@ornio.no';
 
 export const smtpConfig = {
     host: MAIL_HOST,
