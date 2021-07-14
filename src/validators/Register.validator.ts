9160d9afc040358165d952f33aa8ee31544f7e1b
Fitim Bytyqi <fitim.bytyqi@ornio.no>
2021-07-14 21:28:20 +0200

    REST-7 added validations for whitespaces

--- src/validators/Register.validator.ts
+++ src/validators/Register.validator.ts
@@ -1,8 +1,8 @@
 import Joi from 'joi';
 
 export const RegisterValidator = Joi.object().keys({
-    first_name: Joi.string().required(),
-    last_name: Joi.string().required(),
-    email: Joi.string().email().required(),
+    first_name: Joi.string().regex(/^\S+$/).required(),
+    last_name: Joi.string().regex(/^\S+$/).required(),
+    email: Joi.string().email().trim().required(),
     password: Joi.string().min(8).required(),
 });
