38bac56d4f88561e2b6a819355a7f12a927cbaa4
Fitim Bytyqi <fitim.bytyqi@ornio.no>
2021-07-15 09:32:08 +0200

    REST-13 added trim to password field

--- src/validators/Login.validator.ts
+++ src/validators/Login.validator.ts
@@ -2,5 +2,5 @@ import Joi from 'joi';
 
 export const LoginValidator = Joi.object().keys({
     email: Joi.string().email().trim().required(),
-    password: Joi.string().min(8).required(),
+    password: Joi.string().trim().min(8).required(),
 });
