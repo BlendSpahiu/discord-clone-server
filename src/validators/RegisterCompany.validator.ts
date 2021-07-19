5bc8f4e4efc5f10696dcb3687b1a48e4369c2a70
Fitim Bytyqi <fitim.bytyqi@ornio.no>
2021-07-19 09:59:13 +0200

    REST-52 Modified validator

--- src/validators/RegisterCompany.validator.ts
+++ src/validators/RegisterCompany.validator.ts
@@ -3,7 +3,12 @@ import Joi from 'joi';
 export const RegisterCompanyValidator = Joi.object().keys({
     first_name: Joi.string().regex(/^\S+$/).min(2).required(),
     last_name: Joi.string().regex(/^\S+$/).min(2).required(),
-    email: Joi.string().email().trim().required(),
+    email: Joi.string()
+        .email({
+            tlds: { allow: false },
+        })
+        .trim()
+        .required(),
     password: Joi.string().trim().min(8).required(),
     company_name: Joi.string().min(3).required(),
     business_number: Joi.number().min(3).required(),
