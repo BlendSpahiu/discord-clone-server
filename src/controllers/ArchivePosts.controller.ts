b8dfb06003282c79d7b3b3be874674bdbea091fb
Fitim Bytyqi <fitim.bytyqi@ornio.no>
2021-07-30 09:47:57 +0200

    REST-8 Added Post model and archive post controller

---
+++ src/controllers/ArchivePosts.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction, Router } from 'express';
+import PostModel from '../models/Post.model';
+import { ok } from '../utils';
+
+export const ArchivePostsController: Router = Router();
+
+ArchivePostsController.post('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // get the date 30 days ago
+        const date = new Date();
+        const dateInSeconds = new Date().setDate(date.getDate() - 30);
+        // get the timestamp version
+        const expiryDate = new Date(dateInSeconds);
+
+        // find posts whose date is older than 30 days
+        const posts = await PostModel.query()
+            .update({ is_archived: true })
+            .where('created_at', '<', expiryDate)
+            .orWhere('created_at', '=', expiryDate);
+
+        return ok({ posts }, 200);
+    } catch (err) {
+        next(err);
+    }
+});
